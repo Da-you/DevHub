40676f4cc266bc2ec22084f85336a6148b79737d
JoHyunwoo <jhw3874@gmail.com>
2025-01-13 15:07:11 +0900

    [#17] 1차 리팩토링
    - csv 파일을 1차적으로 읽고 정제해 RDB 저장용 파일 생성
    (55만건의 데이터를 정제에 3932ms 소요

--- src/main/java/com/hw/DevHub/global/constant/PathConst.java
+++ src/main/java/com/hw/DevHub/global/constant/PathConst.java
@@ -1,10 +1,15 @@
 package com.hw.DevHub.global.constant;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PathConst {
 
-    @Value("${root.path}")
     public static String PATH_ROOT;
 
+    @Value("${root.path}")
+    public void setPathRoot(String pathRoot) {
+        PATH_ROOT = pathRoot;
+    }
 }
--- src/main/java/com/hw/DevHub/infra/reader/CsvFileUtils.java
+++ src/main/java/com/hw/DevHub/infra/reader/CsvFileUtils.java
@@ -5,40 +5,41 @@ import static com.hw.DevHub.global.constant.PathConst.PATH_ROOT;
 import com.hw.DevHub.domain.cafe.dao.CafeRepository;
 import com.hw.DevHub.domain.cafe.domain.Cafe;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvException;
 import com.opencsv.exceptions.CsvValidationException;
 import jakarta.transaction.Transactional;
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.endpoints.internal.Value.Str;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CsvFileReader {
+public class CsvFileUtils {
 
     private final CafeRepository cafeRepository;
-    public String PATH = PATH_ROOT + "/cafe_list_seoul_202409.csv";
+    public String PATH = PATH_ROOT + "/cafe_list.csv";
 
     @Transactional
-    @EventListener(ApplicationReadyEvent.class)
+//    @EventListener(ApplicationReadyEvent.class)
     public void readCsv() {
         log.info("데이터 저장 시작");
         CSVReader reader = null;
         long startTime = System.currentTimeMillis();
         try {
             reader = new CSVReader(new FileReader(PATH));
-            String[] headers = reader.readNext(); // 헤더를 읽음
+            String[] headers = reader.readNext();
             List<String[]> rows = reader.readAll();
             List<Cafe> cafeList = new ArrayList<>();
             for (int i = 0; i < rows.size(); i++) {
@@ -66,6 +67,53 @@ public class CsvFileReader {
                 }
             }
         }
+    }
+
+    @Transactional
+    @EventListener(ApplicationReadyEvent.class)
+    public void refactorCSV() {
+        log.info("csv read start");
+        CSVReader reader = null;
+        CSVWriter writer = null;
+        long startTime = System.currentTimeMillis();
+        try {
+            reader = new CSVReader(new FileReader(PATH));
+            writer = new CSVWriter(new FileWriter(PATH_ROOT + "/cafe_list_gyeong-gi_202409.csv"));
+
+            String[] headers = {"이름", "도로명주소", "우편번호", "경도", "위도"};
+            writer.writeNext(headers);
+            List<String[]> data = reader.readAll().stream()
+                .filter(row -> "카페".equals(row[8]))
+                .map(row -> new String[]{
+                    row[1] + " " + row[2], // 이름: 상호명 + 지점명
+                    row[30] + " " + row[31], // 도로명주소: 도로명 + 건물명
+                    row[33], // 우편번호
+                    row[37], // 경도
+                    row[38]  // 위도
+                })
+                .toList();
+            writer.writeAll(data);
+            long elapsedTime = System.currentTimeMillis() - startTime; // 소요 시간 계산
+            log.info("데이터 수: {}, 소요 시간: {} ms", data.size(), elapsedTime);
+            System.out.println("CSV 파일 생성 완료: ");
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (CsvException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (writer != null && reader != null) {
+                try {
+                    reader.close();
+                    writer.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
     }
 
     public Cafe convertCsvToCafe(String[] row) {
